72f1b5921299a550385e2325bd8ec65a19b9c26f
cooker <jamoliddingafurov3223@gmail.com>
2025-06-25 00:30:14 +0500

    check user in command start

--- handlers/user_commands.py
+++ handlers/user_commands.py
@@ -3,12 +3,22 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, BotCommand, BotCommandScopeDefault
 from aiogram.fsm.context import FSMContext
 
-from config import config as cfg
+from config import Config as cfg
+from data.database import Database
+
+from markups.reply import main_menu, remove_keyboard
 
 
+db = Database()
 router = Router()
 
 
 @router.message(CommandStart())
-async def start_command(message: Message):
-    await message.answer("salom, botga xush kelibsiz!\n\n")
+async def start_command(message: Message, state: FSMContext) -> None:
+    await state.clear()
+
+    if not await db.user_exists(message.from_user.id):
+        await db.add_user(message.from_user.id)
+        await message.answer("Salom, botga xush kelibsiz!", reply_markup=main_menu())
+    else:
+        await message.answer("Qanday yordam bera olaman!", reply_markup=main_menu())
