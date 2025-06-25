24a9e29bd458e0c6394a89f33018b229a82d4260
cooker <jamoliddingafurov3223@gmail.com>
2025-06-25 09:30:36 +0500

    added sub checker function

--- midlewares/check_sub.py
+++ midlewares/check_sub.py
@@ -10,26 +10,35 @@ from markups.inline import get_channels_markup
 db = Database()
 
 
-class CheckSubscription(BaseMiddleware):
-    async def __call__(
-            self,
-            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-            event: Message,
-            data: Dict[str, Any]
-    ) -> Any:
-        channels = await db.get_chanels()
+# check_sub.py
+async def check_user_subscription(bot, user_id):
+    channels = await db.get_chanels()
+    not_subscribed = []
 
-        not_subscribed = []
-        if channels:
-            for name, link, member in channels:
-                try:
-                    chat_member = await event.bot.get_chat_member(chat_id=member, user_id=event.from_user.id)
-                    if chat_member.status not in ['member', 'administrator', 'creator']:
-                        not_subscribed.append((name, link))
-                except:
-                    print(f"Error checking subscription for {name} ({link})")
-        
+    if channels:
+        for name, link, member in channels:
+            try:
+                chat_member = await bot.get_chat_member(chat_id=member, user_id=user_id)
+                if chat_member.status not in ['member', 'administrator', 'creator']:
+                    not_subscribed.append((name, link))
+            except Exception as e:
+                print(f"Error checking subscription for {name} ({link}): {e}")
+
+    return not_subscribed
+
+
+
+from .check_sub import check_user_subscription
+
+class CheckSubscription(BaseMiddleware):
+    async def __call__(self, handler, event: Message, data):
+        not_subscribed = await check_user_subscription(event.bot, event.from_user.id)
+
         if not_subscribed:
-            await event.answer("Botdan foydalanish uchunkanallarga obuna bo'lishingiz kerak", reply_markup=get_channels_markup(not_subscribed))
+            await event.answer(
+                "Botdan foydalanish uchun kanallarga obuna bo'lishingiz kerak:",
+                reply_markup=get_channels_markup(not_subscribed)
+            )
             return
+
         return await handler(event, data)
