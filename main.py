3398948e7d78695470567ee06a189bde6cb543ce
cooker <jamoliddingafurov3223@gmail.com>
2025-06-25 09:31:18 +0500

    added call router

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from handlers import user_commands, user_messages
+from callbacks import user_call
 
 from midlewares.check_sub import CheckSubscription
 
-from config import Config as cfg
+from config import config as cfg
 from data.database import Database
 
 
@@ -24,6 +25,7 @@ async def main():
 
     dp.include_routers(
         user_commands.router,
+        user_call.router,
         user_messages.router,
     )
 
