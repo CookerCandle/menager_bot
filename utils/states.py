3624ef0d73fc192ce994f710f0d43fcf472676b9
wacuum <xuligan0425@gmail.com>
2025-06-26 14:12:34 +0500

    states: ChannelAdd, ChannelDelete

--- utils/states.py
+++ utils/states.py
@@ -5,3 +5,10 @@ class Survey(StatesGroup):
     expirience = State()
     problems = State()
     
+class ChannelAdd(StatesGroup):
+    name = State()
+    link = State()
+    member = State()
+
+class ChannelDelete(StatesGroup):
+    name = State()
