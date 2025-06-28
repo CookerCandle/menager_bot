fb995356b9d8c725569e40f2ab490796ab03e05b
wacuum <xuligan0425@gmail.com>
2025-06-28 22:07:11 +0500

    maked module

--- data/create_link.py
+++ data/create_link.py
@@ -1,7 +1,7 @@
 from aiogram import Bot
 
 from config import config as cfg
-from markups.reply import main_menu
+
 
 async def create_link(user_id: int, bot: Bot):
     try:
@@ -10,13 +10,10 @@ async def create_link(user_id: int, bot: Bot):
             member_limit=1,
             name=f"Invite for {user_id}"
         )
-        await bot.send_message(
-            chat_id=int(user_id),
-            text=f"Tabriklaymiz, siz guruhga qabul qilindingiz. Shu link orqali qo'shilsangiz bo'ladi: {invite.invite_link}",
-            reply_markup=main_menu()
-        )
+        return invite
     except Exception as e:
         await bot.send_message(
             chat_id=cfg.log_group,
             text=f"Foydalanuvchiga link yaratilmadi --{user_id}--: {e}"
         )
+        return None
